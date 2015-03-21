0854ebeedeedfc0103e348131f91b6aeb95f6d4a
Florent Delgrange <florent.delgrange@gmail.com>
2015-03-21 20:31:22 +0100

    Creation d'un fichier de test Implementation de la methode "detection" qui detecte si la table existe et si les attributs existent egalement dans cette table

--- bdd.py
+++ bdd.py
@@ -1,6 +1,46 @@
 import sqlite3
 
 class Bdd(object):
-	def __init__(self, bdd = None):
-		self.bdd = bdd
-		self.df = []
+    def __init__(self, bdd_name):
+        self.conn = sqlite3.connect(bdd_name+'.db')
+        self.funcDep = []
+
+    def addDep(self, triplet):
+        self.funcDep.append(triplet)
+
+    def detection(self,triplet):
+        cur = self.conn.cursor()
+        #Test n1 : est-ce que la table (triplet[0]) existe ?
+        boolean = False
+        for raw in cur.execute("select name from sqlite_master where type = 'table'"):
+            if(raw[0]==triplet[0]):
+                boolean=True
+                break
+        if boolean==False :
+            return boolean
+
+        #Test n2 : est-ce que les attributs sont bien tous dans cette table ?
+        list = split_str(triplet[1])
+        list.append(triplet[2])
+        for raw in cur.execute("PRAGMA table_info("+triplet[0]+")"):
+            if raw[1] not in list :
+                return False;
+        
+
+
+
+
+
+def split_str(str):
+    list=[]
+    word = ''
+    for i in range(len(str)):
+        if i==len(str)-1 or str[i] == ' ':
+            if i==len(str)-1:
+                word+=str[i]
+            list.append(word)
+            word=''
+        else:
+            word+=str[i]
+    return list
+
---
+++ test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sqlite3 as lite
+import sys
+from bdd import *
+
+con = lite.connect('test.db')
+with con:
+    cur = con.cursor()
+    line = []
+    cur = con.cursor()
+
+    for raw in cur.execute("select name from sqlite_master where type = 'table'"):
+        line.append(raw)
+        if raw[0]=='Cars':
+            print 'vrai'
+        print raw[0]
+    print line
+    for raw in cur.execute("PRAGMA table_info(Cars)"):
+         print raw[1],
+    print #retour a la ligne
+
+print(split_str("michel ma belle sont des mots qui vont tres bien ensembles"))
