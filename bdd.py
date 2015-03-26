f4f5f2c18dfb26bc4d226d90187e30fbc12c8e8d
Martin Lempereur <martin.lempereur@student.umons.ac.be>
2015-03-26 19:46:54 +0100

    Implmentation interface console

--- bdd.py
+++ bdd.py
@@ -1,8 +1,12 @@
 import sqlite3 as lite
+import sys
+
 from normalisation import *
 
 class Bdd(object):
     def __init__(self, bdd_name):
+        print("Type \'help\' to know how use SGBD")
+        print("Type \'exit\' to quit SGBD")
         self.conn = lite.connect(bdd_name+'.db')
         with self.conn:
             cur = self.conn.cursor()
@@ -13,6 +17,51 @@ class Bdd(object):
                 cur.execute("CREATE TABLE FuncDep(name TEXT, X TEXT, A TEXT )")
                 self.conn.commit()
             cur.close()
+    def run(self):
+        input = raw_input("SGBD>>")
+        if input == 'exit':
+            print("Good Bye")
+            return False
+        elif input == "help":
+            self.showHelp()
+            return True
+
+        else:
+            self.execute(input.split())
+        return True
+
+
+    def execute(self,command):
+        if 'showTables' in command:
+            tableList = self.get_tables()
+            for table in tableList:
+                print table
+        elif 'showDep' in command:
+            depList = self.funcDep()
+            if len(depList) <= 0:
+                print("No dependencies")
+            else:
+                for dep in depList:
+                    print dep
+        elif 'addDep' in command:
+            print("Dependence is like X -> A")
+            x = raw_input("Enter X : ")
+            a = raw_input("Enter A : ")
+            self.add_dep((command[1],x,a,))
+        elif 'showAtt' in command:
+            attList = self.get_attributes(command[1])
+            for att in attList:
+                print att
+        elif 'showLogDep' in command:
+            depList = self.get_logical_consequence()
+            if len(depList) <= 0:
+                print("No dependencies")
+            else:
+                for dep in depList:
+                    print dep
+        else:
+            print("Command not found")
+            print("Type \'Help\' to know how use SGBD")
 
     def get_tables(self):
         table_list = []
@@ -52,12 +101,12 @@ class Bdd(object):
     def detection(self, triplet):
         #Test n1 : est-ce que la table (triplet[0]) existe ?
         if triplet[0] not in self.get_tables():
-            print "la table n'existe pas"
+            print ("la table n'existe pas")
             return False
         #Test n2 : est ce que les attributs de la dependance fonctionnelle entree en parametre sont bien dans cette table ?
         parameters = split_str(triplet[1]) + [triplet[2]]
         if not included_in(parameters, self.get_attributes(triplet[0])):
-            print "les attributs n'existent pas"
+            print ("les attributs n'existent pas")
             return False
         #Test n3 : est ce que cette df est utile ?
         return not self.is_useless(triplet)
@@ -121,4 +170,16 @@ class Bdd(object):
 
     def is_3NF(self,table):
         return is_3NF(table, self.get_attributes(table), self.funcDep())
+    def showHelp(self):
+        print(10*"#"+"List of commands"+10*"#"+"\n")
+        print(3*"- "+"showTables : show the name of all the tables in the database")
+        print(3*"- "+"showDep : show all the functional dependencies")
+        print(3*"- "+"addDep nameTable : add a functional dependence to nameTable")
+        print(3*"- "+"showAtt nameTable : show the name of all the attributes of nameTable")
+        print(3*"- "+"showLogDep : show all the logical dependencies")
+        print(3*"- "+"findSuperKey nameTable : find all the super keys of nameTable")
+        print(3*"- "+"findKey nameTable : find the key of nameTable")
+        print(3*"- "+"isBcnf nameTable : say if nameTable is in BCNF")
+        print(3*"- "+"is3nf nameTable : say if nameTable is in 3NF\n")
+
 
--- bddTest.py
+++ bddTest.py
@@ -3,7 +3,7 @@ __author__ = 'Delgrange Florent'
 from bdd import *
 
 data_base = Bdd('exams_bdd')
-print data_base.get_tables()
+print (data_base.get_tables())
 data_base.add_dep(("exam2012", "A B","C"))
 data_base.add_dep(("exam2012","A B","D"))
 data_base.add_dep(("exam2012", "G", "E"))
@@ -20,11 +20,13 @@ data_base.add_dep(('exam2013', 'A B E', 'F'))
 data_base.add_dep(('exam2013', 'A B E', 'C'))
 data_base.add_dep(('exam2013', 'A B', 'C'))
 data_base.add_dep(('exam2013', 'A E', 'F'))
-print data_base.get_attributes("exam2012")
-print data_base.funcDep()
-print data_base.find_super_key('exam2012')
-print data_base.find_key('exam2012')
-print data_base.find_key('exam2013')
-print data_base.is_3NF('exam2012')
-print data_base.is_BCNF('exam2013')
+print (data_base.get_attributes("exam2012"))
+print (data_base.funcDep())
+print (data_base.find_super_key('exam2012'))
+print (data_base.find_key('exam2012'))
+print (data_base.find_key('exam2013'))
+print (data_base.is_3NF('exam2012'))
+print (data_base.is_BCNF('exam2013'))
+while data_base.run():
+    continue
 
