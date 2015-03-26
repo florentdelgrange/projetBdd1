4429e264f5c45eb1d91fcd0d496eb9fa90cb9a92
Florent Delgrange <florent.delgrange@gmail.com>
2015-03-26 17:17:49 +0100

    table_creation est un script qui cree les tables "exam2012" et "exam2013" dans le fichier "exams_bdd.py"

--- table_creation.py
+++ table_creation.py
@@ -1,1 +1,19 @@
 __author__ = 'Flo'
+
+import sqlite3 as lite
+
+con = lite.connect('exams_bdd.db')
+with con:
+
+    cur = con.cursor()
+    cur.execute("CREATE TABLE exam2012(A TEXT, B TEXT, C TEXT, D TEXT, E TEXT, F TEXT, G TEXT, H TEXT)")
+    cur.execute("CREATE TABLE exam2013(A INT, B INT, C INT, D INT, E INT, F INT)")
+    cur.execute("INSERT INTO exam2012 VALUES ('PERRIER', 'FRAISE', 'POUR', 'LES BALAISES', 'PERRIER', 'GRENADINE', 'POUR', 'LES GAMINES')")
+    cur.execute("INSERT INTO exam2012 VALUES (1,2,3,4,5,6,7,8)")
+    for raw in cur.execute("SELECT * FROM exam2012"):
+        print raw
+    for raw in cur.execute("SELECT * FROM exam2013"):
+        print raw
+    con.commit()
+    cur.close()
+con.close()
